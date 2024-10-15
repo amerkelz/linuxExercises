200a652cf3c8fd7c69a5eb1dbff98ea3dfe522ee
ApaBila <salsabila.mahdi@gmail.com>
2024-10-15 14:08:55 -0500

    digits 1st try

--- digits.sh
+++ digits.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+echo {1000..2000} | sed 's/ /\n/g' | { sum=0;
+				       while read n;
+				       do if [[ $n =~ [^0-1] ]]; then
+					      echo "Wrong number..." $n
+					  else
+					      sum=$(($sum + $n))
+					      echo "Right number!" $n
+					  fi
+				       done;
+				       echo $sum; }
