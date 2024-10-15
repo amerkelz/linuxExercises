8dd991833c5a03854add42e6d02ff6ebd451e0cd
Tian <tqiu46@wisc.edu>
2024-10-15 14:18:00 -0500

    school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d "," -f 7 | { sum=0; i=0;  while read n; do sum=$(($sum + $n)) i=$((i+1)); done; avg=$(echo "$sum / $i" | bc -l); echo $avg; } 
