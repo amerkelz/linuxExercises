3a1f3492a0af66ea61cabe40e9d0b11f55e6ae8a
ApaBila <salsabila.mahdi@gmail.com>
2024-10-17 14:05:54 -0500

    #7, rm_n.sh

--- digits.sh
+++ digits.sh
@@ -2,10 +2,8 @@
 echo {1000..2000} | sed 's/ /\n/g' | { sum=0;
 				       while read n;
 				       do if [[ $n =~ ^[01]+$ ]]; then
-					      echo "Right number!" $n
+					      #echo "Right number!" $n
 					      sum=$(($sum + $n))
-					  else
-					      echo "Wrong number..." $n
 					  fi
 				       done;
 				       echo $sum; }
--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# Removes all files in directory <dir> with a size greater than <n> bytes
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <dir> <n> that removes all files in directory dir larger than <n> bytes" 1>&2
+    exit 0
+fi
+
+dir=$1
+n=$2c # add c for bytes
+find $dir -size $n -type f -exec rm {} \;
